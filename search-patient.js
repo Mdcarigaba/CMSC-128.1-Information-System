59083809ba8bf47a8a90c74f9fc45a65df457ac4
lowwisz <lcmelon@up.edu.ph>
2022-11-10 11:07:31 +0800

    added table w filler content

---
+++ search-patient.js
@@ -0,0 +1,124 @@
+let recordsData = [
+    {
+        cifNo: 'CIF-1',
+        labNo: 'LAB-1200',
+        lastName: 'dela Cruz',
+        firstName: 'Juan',
+        middleName: 'Juancho',
+        gender: 'M',
+        age: '20',
+        dateEncoded: '11/09/2022',
+        dateCollected: '11/07/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    },
+    {
+        cifNo: 'CIF-2',
+        labNo: 'LAB-1201',
+        lastName: 'Cruz',
+        firstName: 'Juana',
+        middleName: 'Ibarra',
+        gender: 'F',
+        age: '40',
+        dateEncoded: '10/13/2022',
+        dateCollected: '10/05/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    }, 
+    {
+        cifNo: 'CIF-3',
+        labNo: 'LAB-1202',
+        lastName: 'Santos',
+        firstName: 'Maria Luna',
+        middleName: 'Castro',
+        gender: 'F',
+        age: '38',
+        dateEncoded: '08/12/2022',
+        dateCollected: '07/29/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    },
+    {
+        cifNo: 'CIF-4',
+        labNo: 'LAB-1203',
+        lastName: 'Sanchez',
+        firstName: 'Maria Clara',
+        middleName: 'Ibarra',
+        gender: 'F',
+        age: '65',
+        dateEncoded: '08/12/2022',
+        dateCollected: '07/29/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    },
+    {
+        cifNo: 'CIF-5',
+        labNo: 'LAB-1204',
+        lastName: 'Rizal',
+        firstName: 'Crisostomo',
+        middleName: 'Jose',
+        gender: 'M',
+        age: '72',
+        dateEncoded: '08/12/2022',
+        dateCollected: '07/29/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    },
+    {
+        cifNo: 'CIF-5',
+        labNo: 'LAB-1205',
+        lastName: 'Luna',
+        firstName: 'Juan',
+        middleName: 'Lito',
+        gender: 'M',
+        age: '35',
+        dateEncoded: '08/12/2022',
+        dateCollected: '07/29/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    },
+    {
+        cifNo: 'CIF-6',
+        labNo: 'LAB-1206',
+        lastName: 'Reyes',
+        firstName: 'Harrison',
+        middleName: 'Gregorio',
+        gender: 'M',
+        age: '18',
+        dateEncoded: '08/12/2022',
+        dateCollected: '07/29/2022',
+        validationStatus: 'Validated',
+        finalClassification: 'Suspect'
+    }
+];
+
+window.onload = () => {
+    loadTableData(recordsData);
+};
+
+loadTableData(recordsData);
+
+function loadTableData(recordsData) {
+    const tableBody = document.getElementById('tableData');
+    let dataHTML = '';
+
+    for(let records of recordsData) {
+        dataHTML += `<tr>
+                        <td>${records.cifNo}</td>
+                        <td>${records.labNo}</td>
+                        <td>${records.lastName}</td>
+                        <td>${records.firstName}</td>
+                        <td>${records.middleName}</td>
+                        <td>${records.gender}</td>
+                        <td>${records.age}</td>
+                        <td>${records.dateEncoded}</td>
+                        <td>${records.dateCollected}</td>
+                        <td>${records.validationStatus}</td>
+                        <td>${records.finalClassification}</td>
+                    </tr>`;
+    }
+
+    console.log(dataHTML);
+
+    tableBody.innerHTML = dataHTML;
+}
