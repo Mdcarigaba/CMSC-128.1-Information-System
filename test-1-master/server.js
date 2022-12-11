d47b9e16f78bb923b71f503c2050aeb7a82a7029
jdsalongsongan <jdsalongsongan@up.edu.ph>
2022-12-11 15:45:36 +0800

    Update server.js

--- test-1-master/server.js
+++ test-1-master/server.js
@@ -138,8 +138,9 @@ app.post('/insert-cif', async (req, res) => {
     // const body
     const {investigator,
         
-            philhealth_no, firstname, middlename, lastname,
-            age, birthday, occupation, civil_status, nationality,
+            philhealth_no,
+            firstname, middlename, lastname,
+            age, sex, birthday, occupation, civil_status, nationality,
             permanent_hn_bl_lot_buildno, permanent_barangay,
             permanent_muni_city, permanent_province, permanent_region,
             permanent_home_no, permanent_cell_no, permanent_email,
@@ -148,17 +149,22 @@ app.post('/insert-cif', async (req, res) => {
             current_email, workplace_name, workplace_hn_bl_lot_buildno,
             workplace_barangay, workplace_muni_city, workplace_province,
             workplace_region, workplace_home_no, workplace_cell_no,
-            workplace_email, works_in_closed_settings , 
+            workplace_email, works_in_closed_settings,
+            
+            informant_name, informant_contactno,
+            
             
             //add here lang muna hihu
-
-            relation_to_patient, 
-            health_facility_name, health_facility_address,
-            city_mun_origin, province_origin, is_lsi, is_apor_localtraveler,
+        
+            relation_to_patient,
+            
+            health_facility_name, health_facility_address
+            
+            /*city_mun_origin, province_origin, is_lsi, is_apor_localtraveler,
             institution_name, institution_type,
             country_origin, // oh no d ito unique: health_facility_address
             date_interview, client_classification, testing_category, validation_status,
-            /**cif_id */ is_new_case, is_unknown, other,
+            /**cif_id  is_new_case, is_unknown, other,
             symptoms, health_status, case_classification, vaccination, lab_result, chest_image_findings, disposition, exposure,
             health_status_at_consult, case_classification_at_consult,
             date_of_consultation, consultation_facility_name,
@@ -177,13 +183,13 @@ app.post('/insert-cif', async (req, res) => {
             //lab_test: cli_info_id
             test_positive_before, date_specimen_collection, lab_name, test_count,
             //test: lab_test_info_id
-            date_collected, date_released, /*lab_name*/ is_ops, is_nps, is_antigen, reason_antigen, kit_brand, done_antibody, other_test,
+            date_collected, date_released, /*lab_name is_ops, is_nps, is_antigen, reason_antigen, kit_brand, done_antibody, other_test,
             //outcome: cli_info_id
             is_active, is_recovered, date_recovered, is_dead, date_death, immediate_cause_death, underlying_cause_death, antecedent_cause_death, contributory_cause_death,
             //contact_tracing: cif_id
             has_exposure_people, date_of_contact, has_exposure_place,
             //international_contact: tracing_id
-            have_ongoing_transmission, travel_date_from, travel_date_to, /*country_origin,*/ airline_vessel_name, airline_vessel_number, departure_date, arrival_date,
+            have_ongoing_transmission, travel_date_from, travel_date_to, /*country_origin, airline_vessel_name, airline_vessel_number, departure_date, arrival_date,
             //close_contact: tracing_id
             name, contact_number,
             //local_contact: tracing_id
@@ -192,17 +198,17 @@ app.post('/insert-cif', async (req, res) => {
             transport_type, transport_number, origin, destination, //departure_date, arrival_date
             //dru_queue: dru_id
             entry_count, submission_from, submission_to
-
-
-
+            */
             } = req.body;
     var investigator_id = 0;
+    var patient_id = 0;
+    var informant_id = 0;
     await db
     .select('id')
     .from('staff')
     .where('lastname', `${investigator}`)
     .then((data) => {
-        res.json(data);
+        //res.json(data);
         investigator_id = data[0].id;
     })
     .then(
@@ -214,6 +220,7 @@ app.post('/insert-cif', async (req, res) => {
                 middlename: middlename,
                 lastname: lastname,
                 age: age,
+                sex: sex,
                 birthday: birthday,
                 occupation: occupation,
                 civil_status: civil_status,
@@ -245,204 +252,63 @@ app.post('/insert-cif', async (req, res) => {
                 workplace_email: workplace_email, 
                 works_in_closed_settings: works_in_closed_settings
             })
-        }
-    )
-    //do this
-    .then(
-        async () => {
-            await db('')
-            .insert({
-                relation_to_patient: relation_to_patient,
-
-                health_facility_name: health_facility_name,
-                health_facility_address: health_facility_address,
-
-                //lsi_apor_localtraveler: lsi_apor_localtraveler,
-                //patient_id
-                city_mun_origin: city_mun_origin,
-                province_origin: province_origin,
-                is_lsi: is_lsi,
-                is_apor_localtraveler,
-
-                //living_in_close_settings
-                //patient_id
-                institution_name: institution_name,
-                institution_type: institution_type,
-
-                //returing_overseas_filipino
-                //patient_id
-                country_origin: country_origin,
-                health_facility_address: health_facility_address,
-
-                //cif
-                //id
-                //patient_id
-                //investigator_id
-                date_interview: date_interview,
-                client_classification: client_classification,
-                testing_category: testing_category,
-                validation_status: validation_status,
-
-                //cif_type
-                //cif_id
-                is_new_case: is_new_case,
-                is_unknown: is_unknown,
-                //other_cif_type
-
-                //for_update
-                //cif_id
-                symptoms: symptoms,
-                health_status: health_status,
-                case_classification: case_classification,
-                vaccination: vaccination,
-                lab_result: lab_result,
-                chest_image_findings: chest_image_findings,
-                disposition: disposition,
-                exposure: exposure,
-
-                //case_investigation_details
-                //cif_id
-                health_status_at_consult: health_status_at_consult,
-                case_classification_at_consult: case_classification_at_consult,
-
-                //heave_prev_consultation
-                //cid_if
-                date_of_consultation: date_of_consultation,
-                consultation_facility_name: consultation_facility_name,
-
-                //disposition
-                //cid_id
-                admitted_in: admitted_in,
-                name_of_facility: name_of_facility,
-                datetime_admission_isolation: datetime_admission_isolation,
-                //other_disposition
-
-                //vaccination_info
-                //cid_id
-                name_of_vaccine: name_of_vaccine,
-                vaccination_date: vaccination_date,
-                dose_number: dose_number,
-                vaccination_facility_name: vaccination_facility_name,
-                vaccination_facility_region: vaccination_facility_region,
-                adverse_effect: adverse_effect,
-
-                //clinical_information
-                //cid_id
-                date_of_onset_illness: date_of_onset_illness,
-                is_pregnant: is_pregnant,
-                date_of_last_mensperiod: date_of_last_mensperiod,
-                is_diagnosed_to_sari: is_diagnosed_to_sari,
-
-                //symptoms
-                //cli_info_id
-                is_asymptomatic: is_asymptomatic,
-                have_fever: have_fever,
-                fever_temp: fever_temp,
-                have_cough: have_cough, 
-                have_general_weakness: have_general_weakness,
-                experiences_fatigue: experiences_fatigue,
-                have_headache: have_headache,
-                have_myalgia: have_myalgia,
-                have_sore_throat: have_sore_throat,
-                have_coryza: have_coryza,
-                have_dyspnea: have_dyspnea,
-                experiences_nausea: experiences_nausea,
-                exp_altered_mental_status: exp_altered_mental_status,
-                exp_anosmia: exp_anosmia,
-                exp_ageusia: exp_ageusia,
-                //other_symptoms,
-
-                //comorbidities
-                //cli_info_id
-                have_gastrointestinal: have_gastrointestinal,
-                have_hypertension: have_hypertension,
-                have_genito_urinary: have_genito_urinary,
-                have_diabetes: have_diabetes,
-                have_neuro_disease: have_neuro_disease,
-                have_heart_disease: have_heart_disease,
-                have_cancer: have_cancer,
-                have_lung_disease: have_lung_disease,
-                //other_comorbidities
-
-                //chest_imaging: cli_info_id
-                done_chest_radiography: done_chest_radiography, 
-                date_chest_radiography: date_chest_radiography, 
-                result_chest_radiography: result_chest_radiography, 
-                done_chest_ct: done_chest_ct, 
-                date_chest_ct: date_chest_ct, 
-                result_chest_ct: result_chest_ct, 
-                done_chest_ultrasound: done_chest_ultrasound, 
-                date_chest_ultrasound: date_chest_ultrasound, 
-                result_chest_ultrasound: result_chest_ultrasound, 
-                other_findings: other_findings, // what if other_chest_findings
-                
-                //lab_test: cli_info_id
-                test_positive_before: test_positive_before, 
-                date_specimen_collection: date_specimen_collection, 
-                lab_name: lab_name, 
-                test_count: test_count,
-
-                //test: lab_test_info_id
-                date_collected: date_collected, 
-                date_released: date_released, /*lab_name*/ 
-                is_ops: is_ops, 
-                is_nps: is_nps, 
-                is_antigen: is_antigen,
-                reason_antigen: reason_antigen, 
-                kit_brand: kit_brand, 
-                done_antibody: done_antibody, 
-                other_test: other_test,
-
-                //outcome: cli_info_id
-                is_active: is_active, 
-                is_recovered: is_recovered, 
-                date_recovered: date_recovered, 
-                is_dead: is_dead, 
-                date_death: date_death, 
-                immediate_cause_death: immediate_cause_death, 
-                underlying_cause_death: underlying_cause_death, 
-                antecedent_cause_death: antecedent_cause_death, 
-                contributory_cause_death: contributory_cause_death,
-
-                //contact_tracing: cif_id
-                has_exposure_people: has_exposure_people, 
-                date_of_contact: date_of_contact, 
-                has_exposure_place: has_exposure_place,
-
-                //international_contact: tracing_id
-                have_ongoing_transmission: have_ongoing_transmission, 
-                travel_date_from: travel_date_from,
-                travel_date_to: travel_date_to, /*country_origin,*/ 
-                airline_vessel_name: airline_vessel_name, 
-                airline_vessel_number: airline_vessel_number, 
-                departure_date: departure_date, 
-                arrival_date: arrival_date,
-
-                //close_contact: tracing_id
-                name: name, 
-                contact_number: contact_number,
-
-                //local_contact: tracing_id
-                place_type: place_type, 
-                place_name: place_name, 
-                address: address, //travel_date_from, travel_date_to, have_ongoing_transmission
-                
-                //local_transport: local_contact_id
-                transport_type: transport_type, 
-                transport_number: transport_number, 
-                origin: origin, 
-                destination: destination, //departure_date, arrival_date
-               
-                //dru_queue: dru_id
-                entry_count: entry_count, 
-                submission_from: submission_from, 
-                submission_to: submission_to
-
-
-
+            .returning('id')
+            .then((data) => {
+                //res.json(data);
+                patient_id = data[0].id;
+                //console.log(data)
             })
         }
     )
+    .then(
+        async () => {
+            if(informant_name != 'none'){
+                await db('informant')
+            .insert({
+                name: informant_name,
+                contact_no : informant_contactno
+            })
+            .returning('id')
+            .then((data) => {
+                informant_id = data[0].id
+                //res.json(data)
+            })
+            }
+        }
+    )
+    .then(
+        async () => {
+            await db('patient_informant')
+            .insert({
+                patient_id: patient_id,
+                informant_id: informant_id,
+                relation_to_patient: relation_to_patient
+            })
+            //.returning(['patient_id', 'informant_id', 'relation_to_patient'])
+            /*.then((data) => {
+                //res.json(data);
+            })
+            */
+        }
+    )
+    //skipped hc_w to rof
+    .then(
+        async () => {
+            await db('healthcare_worker')
+            .insert({
+                patient_id: patient_id,
+                health_facility_name: health_facility_name,
+                health_facility_address: health_facility_address,
+            })
+            /*
+            .returning(['patient_id', 'health_facility_name', 'health_facility_address'])
+            .then((data) => {
+                res.json(data)
+            })
+            */
+        }
+    )
+    
 })
 
 //for testing
