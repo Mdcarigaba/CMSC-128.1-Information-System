df3db1e5fb0a8f2a00c8742c3c61b655c6ed0b09
lowwisz <lcmelon@up.edu.ph>
2022-12-11 21:24:05 +0800

    made dru table dynamic

--- test-1-master/public/js/druqueueing.js
+++ test-1-master/public/js/druqueueing.js
@@ -1,3 +1,38 @@
-let DRUqueue = [{
-
-}]
+let DRUqueue = [
+    {
+        druNum: 1,
+        dru: 'Cavite City CHO',
+        numEntries: 230,
+        submissionTime: '8:00-8:20'
+    },
+    {
+        druNum: 2,
+        dru: 'Dasmarinas City CHO',
+        numEntries: 187,
+        submissionTime: '8:20-8;40'
+    },
+    {
+        druNum: 3,
+        dru: 'Aadeo RHU',
+        numEntries: 65,
+        submissionTime: '8:40-9:00'
+    },
+    {
+        druNum: 4,
+        dru: 'Tagaytay City CHO',
+        numEntries: 39,
+        submissionTime: '9:00-9:20'
+    },
+    {
+        druNum: 5,
+        dru: 'Dasmarinas City CHO',
+        numEntries: 12,
+        submissionTime: '9:20-9:40'
+    },
+    {
+        druNum: 6,
+        dru: 'Trece Martires CHO',
+        numEntries: 8,
+        submissionTime: '9:40-10:0'
+    }
+]
